25b3b1eba026d86913bf2842127fdc5af3fedd49
Eva Simon <eva.simon@gmail.com>
2017-09-26 13:51:44 -0500

    add comments

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 
-
+// Write a script that prints the numbers 1 to 100 in the console.
 for ( var number = 1; number < 100; number++) {
 
+	// print `FizzBuzz` for multiples of both three and five
 	if ( number % 3 === 0 && number % 5 === 0 ) {
 		console.log("FIZZBUZZ");
+
+	// print 'Buzz' for multiples of five, instead of the number	
 	} else if (number % 5 === 0) {
 		console.log("BUZZ");
+
+	// print `Fizz` for multiples of three, instead of the number	
 	} else if ( number % 3 === 0 ){
 		console.log("FIZZ");
+
 	} else {
 		console.log(number);
+		
 	}
 
 }
